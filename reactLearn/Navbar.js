ad4c65e929e5911ae7c081b10edb575601a6423a
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-03-16 00:14:37 +0300

    Create Navbar.js

---
+++ reactLearn/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import reactLogo from "../images/react-logo.jpg"
+function Navbar(){
+    return(
+        <div className="navbar-place-holder">
+            <h1 className="navbar-logo-text">ReactFacts</h1>
+            <h1 className="navbar-title">React Course - Project 1</h1>
+
+        </div>
+    )
+}
+
+export default Navbar;
+
