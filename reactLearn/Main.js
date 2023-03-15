1bceee0a8abb7cae06ac94d7aecbd318fd82c9d7
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-03-16 00:14:04 +0300

    Create Main.js

---
+++ reactLearn/Main.js
@@ -0,0 +1,26 @@
+import React from "react"
+import reactLogo from "../images/react-logo.jpg"
+
+
+function Main(){
+    return (
+
+
+
+            <div className="mainContent-place-holder">
+
+                <h1 className="mainContent-text">Fun facts about React</h1>
+
+                <ul className="mainContent-list">
+                    <li><span>Lorem ipsum dolor sit amet.</span></li>
+                    <li><span>Lorem ipsum dolor sit amet.</span></li>
+                    <li><span>Lorem ipsum dolor sit amet.</span></li>
+                    <li><span>Lorem ipsum dolor sit amet.</span></li>
+                    <li><span>Lorem ipsum dolor sit amet.</span></li>
+                </ul>
+
+            </div>
+    )
+}
+
+export default Main
